7190beb78be1965f07e10a8c2e2bee42eb0d9554
jrsdn25 <jdguce93@gmail.com>
2024-10-14 00:10:50 +0800

    Add files via upload

---
+++ Simple Calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    cout<<"======================"<<endl;
+    cout<<"  SIMPLE CALCULATOR"<<endl;
+    cout<<"======================"<<endl;
+    cout<<endl;
+    cout<<"Enter First number: "<<endl;
+    cin>>num1;
+    cout<<"Enter Second number; "<<endl;
+    cin>>num2;
+    cout<<endl;
+    cout<<"======================"<<endl;
+    cout<<"        RESULT"<<endl;
+    cout<<"======================"<<endl;
+    cout<<"Addition: "<<num1+num2<<endl;
+    cout<<"Subtraction: "<<num1-num2<<endl;
+    cout<<"Multiplication: "<<num1*num2<<endl;
+    cout<<"Division: "<<num1/num2<<endl;
+    cout<<"======================"<<endl;
+    return 0;
+}
---
+++ Simple Grading System.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double math,scie,fil,eng,phyed,socstudy,arts,result,total;
+    string Fullname;
+    string PassFail;
+    char midin;
+    cout<<"============================"<<"\n";
+    cout<<"   Simple Grading System"<<"\n";
+    cout<<"============================"<<"\n";
+    cout<<"Enter Full name: \n\*Enter last name first. e.g: Doe, John O.\* "<<"\n";
+    getline (cin, Fullname);
+    cout<<"============================"<<"\n";
+    cout<<"Math: ";
+    cin>>math;
+    cout<<"Science: ";
+    cin>>scie;
+    cout<<"Filipino: ";
+    cin>>fil;
+    cout<<"English: ";
+    cin>>eng;
+    cout<<"P.E.: ";
+    cin>>phyed;
+    cout<<"Social Studies: ";
+    cin>>socstudy;
+    cout<<"Arts: ";
+    cin>>arts;
+    cout<<"============================"<<"\n";
+    result = math+scie+fil+eng+phyed+socstudy+arts;
+    total = result / 7;
+    total = round(total*100) / 100.0;
+    PassFail=(total>=75)?"Grade PASSED!":"Grade FAILED.";
+    cout<<"\n";
+    cout<<"============================"<<"\n";
+    cout<<Fullname<<"\n";
+    cout<<"============================"<<"\n";
+    cout<<"Math: "<<math<<"\n";
+    cout<<"Science: "<<scie<<"\n";
+    cout<<"Filipino: "<<fil<<"\n";
+    cout<<"English: "<<eng<<"\n";
+    cout<<"P.E: "<<phyed<<"\n";
+    cout<<"Social Studies: "<<socstudy<<"\n";
+    cout<<"Arts: "<<arts<<"\n";
+    cout<<"============================"<<"\n";
+    cout<<"Overall Grade: "<<total<<"\n";
+    cout<<PassFail<<"\n";
+    cout<<"============================";
+    return 0;
+}
